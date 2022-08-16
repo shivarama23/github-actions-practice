2ab78c88f3e8cea6e5d5523dcb9f9164f3b3f350
shivarama23 <shivarama23@gmail.com>
2022-08-16 02:48:53 +0000

    Added test cases

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Add function
+def add(x, y):
+    """
+    Function to add two numbers
+    """
+    return x + y
+
+
+print(add(2, 3))
---
+++ test_hello.py
@@ -0,0 +1,8 @@
+from hello import add
+
+
+def test_add():
+    """
+    Test case for add function
+    """
+    assert 5 == add(2, 4)
